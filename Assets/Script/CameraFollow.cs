4d643d2669ea4d0bf4e999289da1d2c7fa884b7f
AkiiSauceCode <akii.mmorpg@gmail.com>
2025-11-30 12:41:14 +0800

    The base Player movement and environment

--- Assets/Script/CameraFollow.cs
+++ Assets/Script/CameraFollow.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0f, 2f, -10f);
-    private float smoothTime = 0.1f;
+    public Vector3 offset = new Vector3(0f, 0f, 0f);
+    public float smoothTime = 0.1f;
     private Vector3 velocity = Vector3.zero; 
 
     [SerializeField] private Transform target;
---
+++ Assets/Script/Health.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+   public int maxHealth = 10;
+   public int currentHelth = 10;
+
+   
+}
